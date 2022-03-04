43ab0de75ff38c6c1e7a8f1e6035615091d89164
Łukasz Kędroń <lukaszke@kainos.com>
2022-03-04 12:03:26 +0100

    breaking the logic

--- Dojo.Tests/Tests.cs
+++ Dojo.Tests/Tests.cs
@@ -11,14 +11,20 @@ namespace Dojo.Tests
         }
 
         [Fact]
-        public void T1ListContainingZerosShouldSumToZero()
+        public void ListContainingZerosShouldReturnTrueWhenFindingZero()
         {
             Assert.True(Solution.CanAddUp(new[] {0,0}, 0));
         }
         [Fact]
-        public void T2ListContainingnumShouldSumToNonZero()
+        public void ListContainingSumShouldReturnTrueWhenFindingPositiveNumber()
         {
             Assert.True(Solution.CanAddUp(new[] {1,3}, 4));
+        }
+
+        [Fact]
+        public void ListNotContainingSumShouldReturnFalse()
+        {
+            Assert.False(Solution.CanAddUp(new [] {0, 0}, 4));
         }
     }
 }
--- Dojo/Solution.cs
+++ Dojo/Solution.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Dojo
+﻿namespace Dojo
 {
     public static class Solution
     {
